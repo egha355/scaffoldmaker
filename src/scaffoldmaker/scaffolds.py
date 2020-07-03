51fb9fcca4cc12b9c1d56f92185e80b746434909
Elias Ghadam Soltani <egha355@UoA.auckland.ac.nz>
2020-07-03 15:10:43 +1200

    modified scafold.py

--- src/scaffoldmaker/scaffolds.py
+++ src/scaffoldmaker/scaffolds.py
@@ -27,6 +27,7 @@ from scaffoldmaker.meshtypes.meshtype_3d_lens1 import MeshType_3d_lens1
 from scaffoldmaker.meshtypes.meshtype_3d_ostium1 import MeshType_3d_ostium1
 from scaffoldmaker.meshtypes.meshtype_3d_smallintestine1 import MeshType_3d_smallintestine1
 from scaffoldmaker.meshtypes.meshtype_3d_solidcylinder1 import MeshType_3d_solidcylinder1
+from scaffoldmaker.meshtypes.meshtype_3d_solidcylinder2 import MeshType_3d_solidcylinder2
 from scaffoldmaker.meshtypes.meshtype_3d_solidsphere1 import MeshType_3d_solidsphere1
 from scaffoldmaker.meshtypes.meshtype_3d_sphereshell1 import MeshType_3d_sphereshell1
 from scaffoldmaker.meshtypes.meshtype_3d_sphereshellseptum1 import MeshType_3d_sphereshellseptum1
@@ -65,6 +66,7 @@ class Scaffolds(object):
             MeshType_3d_ostium1,
             MeshType_3d_smallintestine1,
             MeshType_3d_solidcylinder1,
+            MeshType_3d_solidcylinder2,
             MeshType_3d_solidsphere1,
             MeshType_3d_sphereshell1,
             MeshType_3d_sphereshellseptum1,
